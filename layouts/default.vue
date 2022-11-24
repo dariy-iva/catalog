e1f8e0d91d59b5ce75408f248f4e88389fce43e2
Дарья Иванова <dashann@mail.ru>
2022-11-24 13:55:35 +0300

    feat/add font and styles for body

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,45 @@
 <template>
-  <Nuxt/>
+  <div class="page">
+    <Nuxt/>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "default"
+  name: "default",
+
+  head() {
+    return {
+      bodyAttrs: {
+        class: 'body'
+      }
+    }
+  },
 }
 </script>
 
-<style scoped>
+<style>
+@import "static/fonts/open_sans/OpenSans.css";
+
+.body {
+  margin: 0 auto;
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.page {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  --black-color: #000;
+  --white-color: #fff;
+  --brand-black-color: #333;
+  --light-gray-color: #C4C4C4;
+  --gray-color: #F2F2F2;
+  --dark-gray-color: #828282;
+  background-color: var(--white-color);
+}
 
 </style>
